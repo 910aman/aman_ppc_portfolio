555f877fc0ce107f1c132476815a10068ca8a817
Amansingh Chauhan <amanchauhan9426@gmail.com>
2026-05-05 11:48:01 +0530

    removed the two pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout.tsx';
-import Services from './pages/Services.tsx';
-import Contact from './pages/Contact.tsx';
+// import Services from './pages/Services.tsx';
+// import Contact from './pages/Contact.tsx';
 import Home from './pages/Home.tsx';
 import WebPortfolio from './pages/WebPortfolio.tsx';
 import LeadsPortfolio from './pages/LeadsPortfolio.tsx';
@@ -11,11 +11,11 @@ const App = () => (
   <Layout>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/services" element={<Services />} />
+      {/* <Route path="/services" element={<Services />} /> */}
       <Route path="/web-development" element={<WebPortfolio />} />
       <Route path="/leads-generation" element={<LeadsPortfolio />} />
       <Route path="/google-ads" element={<AdsPortfolio />} />
-      <Route path="/contact" element={<Contact />} />
+      {/* <Route path="/contact" element={<Contact />} /> */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </Layout>
