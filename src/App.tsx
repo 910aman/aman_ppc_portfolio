d1e3f2e278c142572eca0e77f2213c98d6090597
Amansingh Chauhan <amanchauhan9426@gmail.com>
2026-05-05 11:35:16 +0530

    added file extention in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
-import Services from './pages/Services';
+import Services from './pages/Services.tsx';
 import WebPortfolio from './pages/WebPortfolio';
 import LeadsPortfolio from './pages/LeadsPortfolio';
 import AdsPortfolio from './pages/AdsPortfolio';
-import Contact from './pages/Contact';
+import Contact from './pages/Contact.tsx';
 
 const App = () => (
   <Layout>
